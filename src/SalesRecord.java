33768d7f81f29181146c76836f378dbde37fd742
James <justjamescreative@outlook.com>
2025-03-10 17:19:31 -0400

    Created a SalesRecord class
    Add a SalesRecord object to hold the parts of the csv

---
+++ src/SalesRecord.java
@@ -0,0 +1,36 @@
+import java.time.YearMonth;
+
+public class SalesRecord {
+
+    private YearMonth date;
+    private int sales;
+
+    public SalesRecord(YearMonth date, int sales) {
+        this.date = date;
+        this.sales = sales;
+    }
+
+    public YearMonth getDate() {
+        return date;
+    }
+
+    public void setDate(YearMonth date) {
+        this.date = date;
+    }
+
+    public int getSales() {
+        return sales;
+    }
+
+    public void setSales(int sales) {
+        this.sales = sales;
+    }
+
+    @Override
+    public String toString() {
+        return "SalesRecord{" +
+                "date=" + date +
+                ", sales=" + sales +
+                '}';
+    }
+}
