8d1122d7f497e241078fd3c958968afd993575e5
James <justjamescreative@outlook.com>
2025-03-11 09:16:01 -0400

    Updated Application

--- src/SalesDataApplication.java
+++ src/SalesDataApplication.java
@@ -1,3 +1,5 @@
+import service.FileService;
+
 public class SalesDataApplication {
 
     public static void main(String[] args) {
--- src/SalesRecord.java
+++ src/SalesRecord.java
@@ -2,20 +2,30 @@ import java.time.YearMonth;
 
 public class SalesRecord {
 
-    private YearMonth date;
+    private int year;
+    private int month;
     private int sales;
 
-    public SalesRecord(YearMonth date, int sales) {
-        this.date = date;
+    public SalesRecord(int year, int month, int sales) {
+        this.year = year;
+        this.month = month;
         this.sales = sales;
     }
 
-    public YearMonth getDate() {
-        return date;
+    public int getYear() {
+        return year;
     }
 
-    public void setDate(YearMonth date) {
-        this.date = date;
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
     }
 
     public int getSales() {
@@ -26,11 +36,4 @@ public class SalesRecord {
         this.sales = sales;
     }
 
-    @Override
-    public String toString() {
-        return "SalesRecord{" +
-                "date=" + date +
-                ", sales=" + sales +
-                '}';
-    }
 }
