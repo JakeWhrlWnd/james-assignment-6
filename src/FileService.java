2328f3f68800c4ae25f3c3989b155cf9a2f578aa
James <justjamescreative@outlook.com>
2025-03-11 01:40:40 -0400

    Updated the Application

--- src/FileService.java
+++ src/FileService.java
@@ -1,24 +1,48 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileService {
 
-    public List<String> read(String fileName) {
-        List<String> list = new ArrayList<>();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM");
+    private static final DateTimeFormatter YEAR_FORMATTER =
+
+    public List<SalesRecord> read(String fileName) {
+        List<SalesRecord> list = new ArrayList<>();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             bufferedReader.readLine();
-            String line;
-            while((line = bufferedReader.readLine()) != null) {
-
-            }
+//            String line;
+//
+//            while ((line = bufferedReader.readLine()) != null) {
+//                String[] parts = line.split(",");
+//                if (parts.length == 2) {
+//                    String[] date = parts[0].split("-");
+//                    YearMonth year = YearMonth.parse(date[1], YEAR_FORMATTER);
+//                    list.add(new SalesRecord(yearMonth, Integer.parseInt(parts[1].trim())));
+//                }
+//            }
+            list = bufferedReader.lines()
+                    .map(line -> line.split(","))
+                    .filter(parts -> parts.length == 2)
+                    .map(date -> date[0].split("-"))
+                    .map(parts -> new SalesRecord(YearMonth.parse(parts[0].trim()), Integer.parseInt(parts[1].trim())))
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             System.out.println("Oops, there was issue reading the file -> Issue " + e.getMessage());
         }
 
         return list;
     }
+
+    // filter
+
+    // print
 }
--- src/SalesDataApplication.java
+++ src/SalesDataApplication.java
@@ -1,33 +1,36 @@
 public class SalesDataApplication {
 
     public static void main(String[] args) {
-        System.out.println("Model 3 Yearly Sales Report");
-        System.out.println("---------------------------");
-        System.out.println("2017 -> ####");
-        System.out.println("2018 -> ####");
-        System.out.println("2019 -> ####");
-        System.out.println();
-        System.out.println("The best month for Model 3 was: yyyy-MM");
-        System.out.println("The worst mont for Model 3 was: yyyy-MM");
+        FileService fileService = new FileService();
 
-        System.out.println("Model S Yearly Sales Report");
-        System.out.println("---------------------------");
-        System.out.println("2016 -> ####");
-        System.out.println("2017 -> ####");
-        System.out.println("2018 -> ####");
-        System.out.println("2019 -> ####");
-        System.out.println();
-        System.out.println("The best month for Model S was: yyyy-MM");
-        System.out.println("The worst mont for Model S was: yyyy-MM");
-
-        System.out.println("Model X Yearly Sales Report");
-        System.out.println("---------------------------");
-        System.out.println("2016 -> ####");
-        System.out.println("2017 -> ####");
-        System.out.println("2018 -> ####");
-        System.out.println("2019 -> ####");
-        System.out.println();
-        System.out.println("The best month for Model X was: yyyy-MM");
-        System.out.println("The worst mont for Model X was: yyyy-MM");
+        System.out.println(fileService.read("C:\\Users\\JustJ\\OneDrive\\git\\james-assignment-6\\model3.csv"));
+//        System.out.println("Model 3 Yearly Sales Report");
+//        System.out.println("---------------------------");
+//        System.out.println("2017 -> ####");
+//        System.out.println("2018 -> ####");
+//        System.out.println("2019 -> ####");
+//        System.out.println();
+//        System.out.println("The best month for Model 3 was: yyyy-MM");
+//        System.out.println("The worst mont for Model 3 was: yyyy-MM");
+//
+//        System.out.println("Model S Yearly Sales Report");
+//        System.out.println("---------------------------");
+//        System.out.println("2016 -> ####");
+//        System.out.println("2017 -> ####");
+//        System.out.println("2018 -> ####");
+//        System.out.println("2019 -> ####");
+//        System.out.println();
+//        System.out.println("The best month for Model S was: yyyy-MM");
+//        System.out.println("The worst mont for Model S was: yyyy-MM");
+//
+//        System.out.println("Model X Yearly Sales Report");
+//        System.out.println("---------------------------");
+//        System.out.println("2016 -> ####");
+//        System.out.println("2017 -> ####");
+//        System.out.println("2018 -> ####");
+//        System.out.println("2019 -> ####");
+//        System.out.println();
+//        System.out.println("The best month for Model X was: yyyy-MM");
+//        System.out.println("The worst mont for Model X was: yyyy-MM");
     }
 }
