1e87e545e257d9d9c8452b52c495faf9013b3def
James <justjamescreative@outlook.com>
2025-03-12 08:36:17 -0400

    Added to the Service level

--- src/service/DateParsingService.java
+++ src/service/DateParsingService.java
@@ -1,8 +1,13 @@
 package service;
 
+import java.time.YearMonth;
+
 public class DateParsingService {
 
     // convertYear()
-
+    public Integer convertYear(int year) {
+        int convertYear =
+        return null;
+    }
     // convertMonth()
 }
--- src/service/FileService.java
+++ src/service/FileService.java
@@ -1,6 +1,25 @@
 package service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileService {
 
-    // read() - method to return a list of Strings
+    public List<String> readFile(String fileName) {
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            bufferedReader.readLine();
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Oops, there was an issue reading the file. Issue -> " + e.getMessage());
+        }
+        return lines;
+    }
 }
